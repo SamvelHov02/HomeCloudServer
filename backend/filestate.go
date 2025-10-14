dc2807a740018cfcb48b7bfd3d6d70eee863fd4e
Samvel Hovhannisyan <samnar2002@icloud.com>
2025-10-14 23:12:46 +0200

    Implemented Datastructure for Merkle Tree
    Merkle Tree implementation started to tackle #2. The code can build
    a tree and compute the hashes of each file and directory in the Vault.
    Things to implement
    - End point for Client to request the merkle tree
    - Turn the merkle tree to JSON

--- backend/filestate.go
+++ backend/filestate.go
@@ -1,7 +1,7 @@
 package backend
 
 import (
-	"fmt"
+	"crypto/sha256"
 	"log"
 	"os"
 	"path/filepath"
@@ -10,25 +10,25 @@ import (
 const VaultPath = "/Users/samvelhovhannisyan/Documents/dev/Personal/HomeCloud/server/"
 
 type Tree struct {
-	Root     string `json:"root"`
-	RootHash string `json:"hash"`
-	Children []Leaf `json:"children"`
+	Root     string  `json:"root"`
+	RootHash string  `json:"hash"`
+	Children []*Leaf `json:"children"`
 }
 
 type Leaf struct {
-	Category string `json:"type"`
-	Children []Leaf `json:"children"`
-	Name     string `json:"name"`
-	Hash     string `json:"hash"`
+	Category string  `json:"type"`
+	Children []*Leaf `json:"children"`
+	Name     string  `json:"name"`
+	Hash     string  `json:"hash"`
 }
 
 func (t *Tree) Init() {
-	t.Root = "Vault"
+	t.Root = VaultPath + "Vault"
 }
 
 func (t *Tree) Build() {
 	// Want to recurse the Vault for each level create a Leaf
-	t.Children = []Leaf{}
+	t.Children = []*Leaf{}
 
 	contents, err := os.ReadDir(t.Root)
 
@@ -42,26 +42,52 @@ func (t *Tree) Build() {
 		if entry.IsDir() {
 			l := &Leaf{
 				Category: "dir",
-				Name:     entry.Name(),
-				Children: []Leaf{},
+				Name:     path,
+				Children: []*Leaf{},
 			}
-			t.Children = append(t.Children, *GetAllNames(path, l))
+			t.Children = append(t.Children, GetAllNames(path, l))
 		} else {
-			t.Children = append(t.Children, Leaf{
+			t.Children = append(t.Children, &Leaf{
 				Category: "file",
-				Name:     entry.Name(),
+				Name:     path,
 			})
 		}
 	}
-
 }
 
-func Test() {
-	fmt.Println("Test")
+func (t *Tree) ComputeHash() {
+	l := &Leaf{
+		Category: "dir",
+		Name:     t.Root,
+		Children: t.Children,
+	}
+
+	t.RootHash = l.ComputeHash()
+}
+
+func (l *Leaf) ComputeHash() string {
+	if l.Category == "file" {
+		data, err := os.ReadFile(l.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		hashData := sha256.Sum256(data)
+		hashStr := string(hashData[:])
+		return hashStr
+	}
+
+	ChildHashes := []byte{}
+	for _, child := range l.Children {
+		ChildHashes = append(ChildHashes, []byte(child.ComputeHash())...)
+	}
+
+	hash := sha256.Sum256(ChildHashes)
+
+	return string(hash[:])
 }
 
 func GetAllNames(root string, l *Leaf) *Leaf {
-	l.Children = []Leaf{}
+	l.Children = []*Leaf{}
 	contents, err := os.ReadDir(root)
 
 	if err != nil {
@@ -74,14 +100,14 @@ func GetAllNames(root string, l *Leaf) *Leaf {
 		if entry.IsDir() {
 			l2 := &Leaf{
 				Category: "dir",
-				Name:     entry.Name(),
-				Children: []Leaf{},
+				Name:     path,
+				Children: []*Leaf{},
 			}
-			l.Children = append(l.Children, *GetAllNames(path, l2))
+			l.Children = append(l.Children, GetAllNames(path, l2))
 		} else {
-			l.Children = append(l.Children, Leaf{
+			l.Children = append(l.Children, &Leaf{
 				Category: "file",
-				Name:     entry.Name(),
+				Name:     path,
 			})
 		}
 	}
--- backend/filestate_test.go
+++ backend/filestate_test.go
@@ -13,18 +13,44 @@ func TestTreeBuilder(t *testing.T) {
 
 	expected := Tree{
 		Root: "/Users/samvelhovhannisyan/Documents/dev/Personal/HomeCloud/server/Vault",
-		Children: []Leaf{
-			{Category: "dir", Name: VaultPath + "/TestFolder", Children: []Leaf{
-				{Category: "file", Name: VaultPath + "/TestFolder/test3.md"},
+		Children: []*Leaf{
+			{Category: "dir", Name: VaultPath + "Vault/TestFolder", Children: []*Leaf{
+				{Category: "file", Name: VaultPath + "Vault/TestFolder/test3.md"},
 			}},
-			{Category: "file", Name: VaultPath + "/test1.md"},
-			{Category: "file", Name: VaultPath + "/test2.md"},
+			{Category: "file", Name: VaultPath + "Vault/test1.md"},
+			{Category: "file", Name: VaultPath + "Vault/test2.md"},
 		},
 	}
 
 	if !reflect.DeepEqual(tr, expected) {
+		fmt.Printf("Expected %+v\n", expected.Children[1])
+		fmt.Println("--------------------------")
+		fmt.Printf("Expected %+v\n", tr.Children[1])
+		t.Errorf("Failed")
+	}
+}
+
+func TestComputeHashes(t *testing.T) {
+	expected := Tree{
+		Root: "/Users/samvelhovhannisyan/Documents/dev/Personal/HomeCloud/server/Vault",
+		Children: []*Leaf{
+			{Category: "dir", Hash: "cd372fb85148700fa88095e3492d3f9f5beb43e555e5ff26d95f5a6adc36f8e6", Name: VaultPath + "/TestFolder", Children: []*Leaf{
+				{Category: "file", Name: VaultPath + "/TestFolder/test3.md", Hash: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"},
+			}},
+			{Category: "file", Name: VaultPath + "/test1.md", Hash: "44db641ce3deea2461f7110d99643c7df0b9c4e4d1f56883df96f52666cd1ba6"},
+			{Category: "file", Name: VaultPath + "/test2.md", Hash: "0ef8d27f630a07b45d00f8e3c0018dc7232bc12707831d8c3c76e2364332dbdb"},
+		},
+		RootHash: "d8e9274b667892d4d9ff0c6a3089ab7978264242e897746171dc82c6fbb7b43a",
+	}
+
+	actual := Tree{}
+	actual.Init()
+	actual.Build()
+	actual.ComputeHash()
+
+	if !reflect.DeepEqual(actual, expected) {
 		fmt.Printf("Expected %+v\n", expected)
 		fmt.Println("--------------------------")
-		fmt.Printf("Expected %+v\n", tr)
+		fmt.Printf("Expected %+v\n", actual)
 	}
 }
